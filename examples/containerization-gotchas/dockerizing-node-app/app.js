475839fb5fc0e6f8f6d5b3a0f7e45f0d4e6d9af4
Joshua Mabina <mabinajoshua@gmail.com>
2019-06-30 17:03:51 +0300

    Clean up

--- examples/containerization-gotchas/dockerizing-node-app/app.js
+++ examples/containerization-gotchas/dockerizing-node-app/app.js
@@ -1,9 +1,10 @@
+
 'use strict';
 
 const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => res.send('Hello world!'));
+app.get('/', (req, res) => res.send('<h1>It works! Exciting!!</h1>'));
 
 app.listen(PORT, () => console.log(`App runs on port: ${PORT}`));
